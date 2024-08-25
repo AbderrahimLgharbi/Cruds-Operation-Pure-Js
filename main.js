6d8cef3765c59e1b2227bb65abde197f4d459b63
Abderrahim LGHARBI <abderrahimdev@gmail.com>
2024-08-25 07:15:16 +0100

    add read products and delete one product

--- main.js
+++ main.js
@@ -8,6 +8,7 @@ let count = document.getElementById('count');
 let category = document.getElementById('category');
 let create = document.getElementById("btnCreate");
 
+// localStorage.removeItem("products")
 
 function getPrice() {
     if (price.value != "") {
@@ -44,5 +45,53 @@ create.addEventListener("click", function () {
     }
     data = [...data, newData];
     localStorage.setItem('products', JSON.stringify(data));
-    console.log(data)
+    clearData();
+    showData();
 })
+
+function clearData() {
+    title.value = "";
+    price.value = "";
+    taxes.value = "";
+    ads.value = "";
+    discount.value = "";
+    total.innerHTML = "";
+    count.value = "";
+    category.value = "";
+}
+
+function showData() {
+    let tableContent = '';
+
+    for (let i = 0; i < data.length; i++) {
+        tableContent += `
+        <tr>
+        <td>${i}</td>
+        <td>${data[i].title}</td>
+        <td>${data[i].price}</td>
+        <td>${data[i].taxes}</td>
+        <td>${data[i].ads}</td>
+        <td>${data[i].discount}</td>
+        <td>${data[i].total}</td>
+        <td>${data[i].category}</td>
+                        <td><input type="button" id="tablebtn" value="Delete" onClick="deleteProd(${i})"/></td>
+    </tr>`;
+    }
+
+    document.getElementById("tbody").innerHTML = tableContent;
+}
+
+
+function deleteProd(indexOfprod) {
+    let confirmDelete = window.confirm("are you sure");
+    if (confirmDelete) {
+        data.splice(indexOfprod, 1);
+        localStorage.setItem("products", JSON.stringify(data));
+        showData();
+    }
+    else {
+        ;
+    }
+}
+
+showData();
